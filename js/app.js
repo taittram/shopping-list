df0b24c2ebf00f94477287ab52f05007ed268d9a
Tai Tram <taittram@yahoo.com>
2018-06-21 13:37:36 -0700

    added livechat snippet in app.js

--- js/app.js
+++ js/app.js
@@ -52,4 +52,12 @@ $(document).ready(function() {
 		e.preventDefault(); $(this).parent().remove()
 	});
 
+     window.__lc = window.__lc || {};
+window.__lc.license = 9874535;
+(function() {
+ var lc = document.createElement('script'); lc.type = 'text/javascript'; lc.async = true;
+ lc.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'cdn.livechatinc.com/tracking.js';
+ var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(lc, s);
+})();
+
 });
